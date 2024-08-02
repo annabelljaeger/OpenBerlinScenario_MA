64c7f46ccaa100ecad7ebde6e7157f423e58d093
Kai Nagel <nagel@vsp.tu-berlin.de>
2024-08-02 19:04:33 +0200

    revert matsim dependency to PR release.  The other version got accidentially pushed.
    add comments to better understand how the various ways to modify config options are parsed

--- src/main/java/org/matsim/run/OpenBerlinScenario.java
+++ src/main/java/org/matsim/run/OpenBerlinScenario.java
@@ -2,8 +2,8 @@ package org.matsim.run;
 
 import com.google.inject.Key;
 import com.google.inject.name.Names;
-import org.matsim.analysis.QsimTimingModule;
-import org.matsim.analysis.TripsAndLegsWriter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.application.MATSimApplication;
@@ -16,6 +16,7 @@ import org.matsim.core.controler.Controler;
 import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
 import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
 import org.matsim.core.router.util.TravelTime;
+import org.matsim.prepare.RunOpenBerlinCalibration;
 import org.matsim.run.scoring.AdvancedScoringConfigGroup;
 import org.matsim.run.scoring.AdvancedScoringModule;
 import org.matsim.simwrapper.SimWrapperConfigGroup;
@@ -29,6 +30,8 @@ public class OpenBerlinScenario extends MATSimApplication {
 
 	public static final String VERSION = "6.3";
 	public static final String CRS = "EPSG:25832";
+
+	private static final Logger log = LogManager.getLogger(RunOpenBerlinCalibration.class);
 
 	@CommandLine.Mixin
 	private final SampleOptions sample = new SampleOptions(10, 25, 3, 1);
@@ -108,10 +111,6 @@ public class OpenBerlinScenario extends MATSimApplication {
 
 		controler.addOverridingModule(new TravelTimeBinding());
 
-		controler.addOverridingModule(new QsimTimingModule());
-
-	//	controler.addOverridingModule(new AdvancedTripsAndLegsWriter());
-
 		if (ConfigUtils.hasModule(controler.getConfig(), AdvancedScoringConfigGroup.class)) {
 			controler.addOverridingModule(new AdvancedScoringModule());
 		}
@@ -130,17 +129,5 @@ public class OpenBerlinScenario extends MATSimApplication {
 			addTravelDisutilityFactoryBinding("freight").to(Key.get(TravelDisutilityFactory.class, Names.named(TransportMode.truck)));
 		}
 	}
-
-	/**
-	 * Verlustzeitkalkulation und Erweiterung des TripsWriter
-
-
-	public static final class AdvancedTripsAndLegsWriter extends AbstractModule {
-		@Override
-		public void install() {
-			addControlerListenerBinding().to(TripsAndLegsWriter.CustomTripsWriterExtension.class);
-		}
-	}
-	 */
 
 }
