554a4c31d375490d35ee572fe859ebadcc71d25e
schlenther <schlenther@vsp.tu-berlin.de>
2024-12-17 11:31:06 +0100

    compute closest centroid

--- src/main/java/org/matsim/analysis/ShpHelperClass.java
+++ src/main/java/org/matsim/analysis/ShpHelperClass.java
@@ -1,5 +1,8 @@
 package org.matsim.analysis;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
 import org.geotools.api.feature.simple.SimpleFeature;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
@@ -7,10 +10,15 @@ import org.locationtech.jts.geom.prep.PreparedGeometry;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.core.utils.geometry.CoordUtils;
 import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.gis.GeoFileReader;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.utils.gis.shp2matsim.ShpGeometryUtils;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ShpHelperClass {
@@ -18,36 +26,59 @@ public class ShpHelperClass {
 
     public static void main(String[] args) {
 
-        List<PreparedGeometry> gruenflachen = ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource("pathTo/myShapeFile.shp"));
+//        List<PreparedGeometry> gruenflachen = ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource("pathTo/myShapeFile.shp"));
 
 
-        List<Coord> activityCoords = new ArrayList<>();
+        Collection<SimpleFeature> allFeatures = GeoFileReader.getAllFeatures(IOUtils.resolveFileOrResource("pathTo/myShapeFile.shp"));
 
-        for (Coord activityCoord : activityCoords) {
 
-            double shortestDistance = Double.MAX_VALUE;
-            PreparedGeometry closestGeometry = null;
+        try {
+            CSVReader reader = new CSVReader(new FileReader("persons.csv"));
 
-            double buffer = 10;
+            String[] personRecord = reader.readNext();
 
-            for (PreparedGeometry preparedGeometry : gruenflachen) {
+            String id = personRecord[0];
+            String homeX = personRecord[3];
+            String homeY = personRecord[4];
 
-                preparedGeometry.getGeometry().buffer(buffer)
+            Coord homeCoord = new Coord(Double.valueOf(homeX),Double.valueOf(homeY));
 
-                double distanceToCentroid = CoordUtils.calcEuclideanDistance(activityCoord, MGC.point2Coord(preparedGeometry.getGeometry().getCentroid()));
+            for (SimpleFeature simpleFeature : allFeatures) {
+
+                Geometry defaultGeometry = (Geometry) simpleFeature.getDefaultGeometry();
+
+                double shortestDistance = Double.MAX_VALUE;
+                Geometry closestGeometry = null;
+
+                double distanceToCentroid = CoordUtils.calcEuclideanDistance(homeCoord, MGC.point2Coord(defaultGeometry.getCentroid()));
                 if (distanceToCentroid < shortestDistance){
                     shortestDistance = distanceToCentroid;
-                    closestGeometry = preparedGeometry;
+                    closestGeometry = defaultGeometry;
                 }
 
-                //
-                for (Coordinate envelopeCoordinate : preparedGeometry.getGeometry().getEnvelope().getCoordinates()) {
 
-                }
+                String name = (String) simpleFeature.getAttribute("name");
+
 
             }
 
+
+
+
+
+
+
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        List<Coord> activityCoords = new ArrayList<>();
 
 
         PreparedGeometry flaeche = gruenflachen.get(0);
