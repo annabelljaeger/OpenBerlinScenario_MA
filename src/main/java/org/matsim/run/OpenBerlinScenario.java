ab3c11d1d9fccb512cb42be6d30812ed65bad445
rakow <rakow@vsp.tu-berlin.de>
2025-01-02 09:24:48 +0100

    remove unneeded module

--- src/main/java/org/matsim/run/OpenBerlinScenario.java
+++ src/main/java/org/matsim/run/OpenBerlinScenario.java
@@ -165,7 +165,6 @@ public class OpenBerlinScenario extends MATSimApplication {
 				}
 			});
 		}
-		controler.addOverridingModule(new PtStop2StopAnalysisModule());
 		controler.addOverridingModule(new PersonMoneyEventsAnalysisModule());
 	}
 
